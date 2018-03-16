a72596bb11da57a503326794ba1d960fbd3e9a67
郭胜强 <guoshengqiang@live.com>
2018-03-16 14:11:36 +0800

    修复d.ts与模块不符的问题

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,2 @@
-declare module textFile {
-    function read(filePath: string, type: string, callback?: Function): Promise<object | string>
-    function write(filePath: string, data: object | string, type: string, callback?: Function): Promise<string | object>
-}
-export default textFile
+export function read(filePath: string, type: string, callback?: Function): Promise<object | string>
+export function write(filePath: string, data: object | string, type: string, callback?: Function): Promise<string | object>
